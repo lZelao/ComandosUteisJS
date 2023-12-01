a597a281fde8b2b3648a50cf9e548d8256a96f5c
Eliseu <esreis123@gmail.com>
2023-12-01 02:22:35 -0200

    Usando o length

---
+++ UsandoOLength.js
@@ -0,0 +1,12 @@
+
+function UsandoOLength(vetor){
+    let teclas = vetor
+    let tamanho =  teclas.length
+
+    console.log(tamanho)
+   
+
+}
+let teste = ["a",'b']
+UsandoOLength(teste)
+// criando uma função que recebe um vetor como parametro e retorna o tamanh dele.
